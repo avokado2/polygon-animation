aa0a864d3187e53d60404b07c786a23ae81d3118
avokado2 <dknechiporenko@gmail.com>
2023-07-13 15:26:45 +0300

    end of lines corrected on Unix

--- src/main/java/Polygon.java
+++ src/main/java/Polygon.java
@@ -10,20 +10,21 @@ public class Polygon extends Figure {
     }
 
     private static final double triangleSquare(Point p1, Point p2, Point p3) {
-        double tr = 0.5*(Math.abs((p2.getX() - p1.getX()) * (p3.getY() - p1.getY()) - (p3.getX()-p1.getX()) * (p2.getY() - p1.getY())));
+        double tr = 0.5 * (Math.abs((p2.getX() - p1.getX()) * (p3.getY() - p1.getY()) - (p3.getX() - p1.getX()) * (p2.getY() - p1.getY())));
         return tr;
     }
 
     private static final Point triangleCenter(Point p1, Point p2, Point p3) {
         double txc_1 = p1.getX() + p2.getX() + p3.getX();
-        double txc = txc_1/3;
+        double txc = txc_1 / 3;
         double tyc_1 = p1.getY() + p2.getY() + p3.getY();
-        double tyc = tyc_1/3;
-        return new Point(txc,tyc);
+        double tyc = tyc_1 / 3;
+        return new Point(txc, tyc);
     }
-    public Polygon copy(){
+
+    public Polygon copy() {
         List<Point> copy1 = new ArrayList<>();
-        for (Point point: points){
+        for (Point point : points) {
             Point copyPoint = point.copy();
             copy1.add(copyPoint);
         }
@@ -34,7 +35,7 @@ public class Polygon extends Figure {
     public double square() {
         double ret = 0;
         for (int i = 0; i < points.size() - 2; i++) {
-            ret += triangleSquare(points.get(0), points.get(i+1), points.get(i+2));
+            ret += triangleSquare(points.get(0), points.get(i + 1), points.get(i + 2));
         }
         return ret;
     }
@@ -62,8 +63,8 @@ public class Polygon extends Figure {
         double cx = 0;
         double cy = 0;
         double a = 0;
-        for (int i =0; i < points.size();i++) {
-            int j = i+1;
+        for (int i = 0; i < points.size(); i++) {
+            int j = i + 1;
             if (j == points.size()) {
                 j = 0;
             }
@@ -75,7 +76,7 @@ public class Polygon extends Figure {
             cy += (yi + yi1) * (xi * yi1 - xi1 * yi);
             a += (xi * yi1 - xi1 * yi);
         }
-        a = a/2;
+        a = a / 2;
         cx = cx / (6 * a);
         cy = cy / (6 * a);
         return new Point(cx, cy);
