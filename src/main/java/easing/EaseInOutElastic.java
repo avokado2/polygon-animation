aae3638bc2e5a3f96d42e6fa5d64b59aa1f16040
avokado2 <dknechiporenko@gmail.com>
2023-07-13 15:18:37 +0300

    end of lines corrected on Unix

--- src/main/java/easing/EaseInOutElastic.java
+++ src/main/java/easing/EaseInOutElastic.java
@@ -4,15 +4,15 @@ public class EaseInOutElastic implements Easing {
 
     @Override
     public double calculateKt(double t) {
-         double c5 = (2 * Math.PI) / 4.5;
+        double c5 = (2 * Math.PI) / 4.5;
 
-         return t == 0
-                    ? 0
-                    : t == 1
-                    ? 1
-                    : t < 0.5
-                    ? -(Math.pow(2, 20 * t - 10) * Math.sin((20 * t - 11.125) * c5)) / 2
-                    : (Math.pow(2, -20 * t + 10) * Math.sin((20 * t - 11.125) * c5)) / 2 + 1;
+        return t == 0
+                ? 0
+                : t == 1
+                ? 1
+                : t < 0.5
+                ? -(Math.pow(2, 20 * t - 10) * Math.sin((20 * t - 11.125) * c5)) / 2
+                : (Math.pow(2, -20 * t + 10) * Math.sin((20 * t - 11.125) * c5)) / 2 + 1;
 
 
     }
