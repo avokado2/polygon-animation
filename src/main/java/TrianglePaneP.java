b6898a45a764b3e10218d043925ed0290ff5b7cc
avokado2 <dknechiporenko@gmail.com>
2023-07-13 15:31:10 +0300

    end of lines corrected on Unix

--- src/main/java/TrianglePaneP.java
+++ src/main/java/TrianglePaneP.java
@@ -10,12 +10,12 @@ import java.util.ArrayList;
 import java.io.*;
 
 
+public class TrianglePaneP extends JFrame {
 
-public class TrianglePaneP extends JFrame{
+    private ObjectOutputStream os;
+    private static final long serialVersionUID = 1L;
 
-    private  ObjectOutputStream os;
-    private static final long serialVersionUID=1L;
-    public TrianglePaneP () throws IOException {
+    public TrianglePaneP() throws IOException {
 
         // создание окна
         super("Example LayeredTest");
@@ -34,7 +34,7 @@ public class TrianglePaneP extends JFrame{
         // выход при закрытии окна
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         // определение многослойной панели
-        JLayeredPane lp=getLayeredPane();
+        JLayeredPane lp = getLayeredPane();
         //for (int i = 0; i < 20; i++) {
 //            java.util.List<Point> points = new ArrayList<Point>();
 //            points.add(new Point(150, 100));
@@ -42,14 +42,14 @@ public class TrianglePaneP extends JFrame{
 //            points.add(new Point(150, 300));
 //            points.add(new Point(100, 200));
 
-            Polygon polygon = Polygon.generate(new Point(300, 300), 20, 200);
-            PolygonComponent polygonComponent = new PolygonComponent(Color.red, polygon);
-            polygonComponent.setBounds(0, 0, 600, 650);
-            lp.add(polygonComponent, JLayeredPane.POPUP_LAYER);
+        Polygon polygon = Polygon.generate(new Point(300, 300), 20, 200);
+        PolygonComponent polygonComponent = new PolygonComponent(Color.red, polygon);
+        polygonComponent.setBounds(0, 0, 600, 650);
+        lp.add(polygonComponent, JLayeredPane.POPUP_LAYER);
 
-            JButton button = new JButton("save/add");
-            lp.add(button, JLayeredPane.POPUP_LAYER);
-            button.setBounds(0, 0, 100, 30);
+        JButton button = new JButton("save/add");
+        lp.add(button, JLayeredPane.POPUP_LAYER);
+        button.setBounds(0, 0, 100, 30);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,24 +59,25 @@ public class TrianglePaneP extends JFrame{
                 } catch (IOException ex) {
                 }
             }
-            });
+        });
 
-            //Triangle t = new Triangle(new Point(150, 100), new Point(100, 200), new Point(200, 200));
-            //TriangleComponent figure = new TriangleComponent(Color.magenta, t);
-            //t.rotate(2*Math.PI * i / 20, center);
-            //figure.setBounds(0, 0, 600, 650);
-            //lp.add(figure, JLayeredPane.POPUP_LAYER);
+        //Triangle t = new Triangle(new Point(150, 100), new Point(100, 200), new Point(200, 200));
+        //TriangleComponent figure = new TriangleComponent(Color.magenta, t);
+        //t.rotate(2*Math.PI * i / 20, center);
+        //figure.setBounds(0, 0, 600, 650);
+        //lp.add(figure, JLayeredPane.POPUP_LAYER);
 
-            //PolygonComponent figure2 = new PolygonComponent(Color.red, polygon);
-            //figure2.setBounds(0, 0, 600, 650);
-            //lp.add(figure2, JLayeredPane.POPUP_LAYER);
+        //PolygonComponent figure2 = new PolygonComponent(Color.red, polygon);
+        //figure2.setBounds(0, 0, 600, 650);
+        //lp.add(figure2, JLayeredPane.POPUP_LAYER);
         //}
         // определение размера и открытие окна
-        setSize(600,650);
+        setSize(600, 650);
         setVisible(true);
 
 
     }
+
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
         UIManager.setLookAndFeel(
                 UIManager.getSystemLookAndFeelClassName());
