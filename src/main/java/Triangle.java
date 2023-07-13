7733874c247194db788ccc63ce6a0063aab5550d
avokado2 <dknechiporenko@gmail.com>
2023-07-13 15:30:10 +0300

    end of lines corrected on Unix

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -3,9 +3,11 @@ public class Triangle extends Figure {
     private Point p1;
     private Point p2;
     private Point p3;
-    public Triangle(){
+
+    public Triangle() {
 
     }
+
     public Triangle(Point p1, Point p2, Point p3) {
 
         this.p1 = p1;
@@ -18,15 +20,17 @@ public class Triangle extends Figure {
 
     @Override
     public double square() {
-        double tr = 0.5*(Math.abs((this.p2.getX() - this.p1.getX()) * (this.p3.getY() - this.p1.getY()) - (this.p3.getX()-this.p1.getX()) * (this.p2.getY() - this.p1.getY())));
+        double tr = 0.5 * (Math.abs((this.p2.getX() - this.p1.getX()) * (this.p3.getY() - this.p1.getY()) - (this.p3.getX() - this.p1.getX()) * (this.p2.getY() - this.p1.getY())));
         return tr;
     }
+
     @Override
     public void rotate(double alpha) {
         this.p1.rotate(alpha);
         this.p2.rotate(alpha);
         this.p3.rotate(alpha);
     }
+
     @Override
     public void rotate(double alpha, Point rotateAxis) {
         this.p1.rotate(alpha, rotateAxis);
@@ -37,10 +41,10 @@ public class Triangle extends Figure {
     @Override
     public Point getCenter() {
         double txc_1 = this.p1.getX() + this.p2.getX() + this.p3.getX();
-        double txc = txc_1/3;
+        double txc = txc_1 / 3;
         double tyc_1 = this.p1.getY() + this.p2.getY() + this.p3.getY();
-        double tyc = tyc_1/3;
-        return new Point(txc,tyc);
+        double tyc = tyc_1 / 3;
+        return new Point(txc, tyc);
 
     }
 
